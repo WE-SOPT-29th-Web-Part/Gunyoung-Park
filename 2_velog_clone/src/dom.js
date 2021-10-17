ff7e92a19882eeb988fca1fb2da50bf4d759bfca
Tekiter <tekiter8@gmail.com>
2021-10-18 03:15:00 +0900

    Create article modal

--- 2_velog_clone/src/dom.js
+++ 2_velog_clone/src/dom.js
@@ -2,7 +2,11 @@ function createDomNode(tagname, props, children = []) {
   const node = document.createElement(tagname);
 
   for (const prop in props) {
-    node[prop] = props[prop];
+    if (prop.startsWith("on")) {
+      node.addEventListener(prop.substring(2), props[prop]);
+    } else {
+      node[prop] = props[prop];
+    }
   }
 
   if (!Array.isArray(children)) {
--- 2_velog_clone/src/index.js
+++ 2_velog_clone/src/index.js
@@ -12,6 +12,7 @@ class ArticleCard {
     likes: 0,
     profileImg: "",
     profileName: "",
+    onClickCard: () => {},
   };
 
   constructor(props) {
@@ -23,42 +24,86 @@ class ArticleCard {
   }
 
   render() {
-    return h("section", { className: "card" }, [
-      h("img", { src: this.props.img }),
-      h("article", {}, [
-        h("h3", {}, [h("a", { href: "#" }, this.props.title)]),
-        h("p", {}, this.props.content),
-        h(
-          "footer",
-          {},
-          `${this.formatTimeStamp()} · ${this.props.commentCount}개의 댓글`
-        ),
-      ]),
-      h("footer", {}, [
-        h("div", { className: "card-footer-author" }, [
-          h("img", { src: this.props.profileImg }),
-          " by ",
+    return h(
+      "section",
+      {
+        className: "card clickable",
+        onclick: () => this.props.onClickCard(),
+      },
+      [
+        h("img", { src: this.props.img }),
+        h("article", {}, [
+          h("h3", {}, [h("a", { href: "#" }, this.props.title)]),
+          h("p", {}, this.props.content),
           h(
-            "span",
-            { className: "card-footer-author-text" },
-            this.props.profileName
+            "footer",
+            {},
+            `${this.formatTimeStamp()} · ${this.props.commentCount}개의 댓글`
           ),
         ]),
-        h("div", { className: "card-footer-like" }, [
-          h("span", { className: "material-icons" }, " favorite "),
-          `${this.props.likes}`,
+        h("footer", {}, [
+          h("div", { className: "card-footer-author" }, [
+            h("img", { src: this.props.profileImg }),
+            " by ",
+            h(
+              "span",
+              { className: "card-footer-author-text" },
+              this.props.profileName
+            ),
+          ]),
+          h("div", { className: "card-footer-like" }, [
+            h("span", { className: "material-icons" }, " favorite "),
+            `${this.props.likes}`,
+          ]),
         ]),
-      ]),
-    ]);
+      ]
+    );
   }
+}
+
+function useModal() {
+  const el = h("div", { className: "modal", onclick: close });
+
+  function show(article) {
+    const articleNode = new ArticleCard({ ...article });
+
+    const content = h("div", { className: "modal-content" }, [
+      articleNode.render(),
+      h("button", { className: "modal-close-button" }, "X"),
+    ]);
+    el.replaceChildren(content);
+
+    el.classList.add("modal-show");
+
+    document.body.style.overflowY = "hidden";
+  }
+
+  function close() {
+    el.classList.remove("modal-show");
+
+    document.body.style.overflowY = "auto";
+  }
+
+  return {
+    el,
+    show,
+    close,
+  };
 }
 
 function useArticleGrid({ container, articleList }) {
   function render() {
+    const modal = useModal();
+
     const children = articleList.map((dat) => {
-      const component = new ArticleCard(dat);
+      const component = new ArticleCard({
+        ...dat,
+        onClickCard: () => modal.show({ ...dat }),
+      });
       return component.render();
     });
+
+    children.push(modal.el);
 
     container.replaceChildren(...children);
   }
