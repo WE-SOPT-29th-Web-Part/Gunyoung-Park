81c6e26c45c995693c9630eeb070e7752c770f20
Tekiter <tekiter8@gmail.com>
2021-10-17 17:48:56 +0900

    Add dropdown state toggle

--- 2_velog_clone/src/dropdown.js
+++ 2_velog_clone/src/dropdown.js
@@ -1,31 +1,58 @@
 export function setNavDropdown() {
   const nodes = document.getElementsByClassName("nav-dropdown");
 
-  for (const node of nodes) {
-    const btns = node.getElementsByClassName("nav-dropdown-button");
+  function setToggle() {
+    for (const node of nodes) {
+      const btns = node.getElementsByClassName("nav-dropdown-button");
 
-    const toggle = (function () {
-      let isOpen = false;
-
-      return function (e) {
-        isOpen = !isOpen;
-
+      const toggle = function (state = null) {
         const dropdownContents = node.getElementsByClassName(
           "nav-dropdown-content"
         );
 
         for (const content of dropdownContents) {
-          if (isOpen) {
+          if (state === null) {
+            content.classList.toggle("dropdown-open");
+          } else if (state === true) {
             content.classList.add("dropdown-open");
-          } else {
+          } else if (state === false) {
             content.classList.remove("dropdown-open");
           }
         }
       };
-    })();
 
-    for (const btn of btns) {
-      btn.addEventListener("click", toggle);
+      for (const btn of btns) {
+        btn.addEventListener("click", () => toggle());
+      }
+
+      node.toggleDropdown = toggle;
     }
   }
+
+  function setSelect() {
+    for (const node of nodes) {
+      const containers = node.getElementsByClassName("nav-dropdown-content");
+
+      for (const container of containers) {
+        for (const child of container.children) {
+          child.addEventListener("click", () => {
+            node.toggleDropdown(false);
+
+            for (const child of container.children) {
+              child.classList.remove("dropdown-selected");
+            }
+            child.classList.add("dropdown-selected");
+
+            const displays = node.getElementsByClassName("dropdown-display");
+            for (const display of displays) {
+              display.textContent = child.textContent;
+            }
+          });
+        }
+      }
+    }
+  }
+
+  setToggle();
+  setSelect();
 }
