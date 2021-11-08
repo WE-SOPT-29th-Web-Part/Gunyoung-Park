2b69826035ef3fe4511b2f18e6e140445e9c69ee
Tekiter <tekiter8@gmail.com>
2021-11-09 02:49:09 +0900

    Init components

--- 5_velog_clone_react/src/App.tsx
+++ 5_velog_clone_react/src/App.tsx
@@ -1,5 +1,16 @@
+import { Route, Routes } from "react-router";
+import { Page404 } from "./pages/404";
+import { Home } from "./pages/Home";
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="*" element={<Page404 />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
